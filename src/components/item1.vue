7b2e0d8ea2849818d39e496cd3a792d9168b0ad6
caizy <caizy2021@163.com>
2022-04-04 01:34:20 +0800

    完成了销售总量图

--- src/components/item1.vue
+++ src/components/item1.vue
@@ -6,40 +6,98 @@
 </template>
 
 <script>
-import { inject, onMounted } from "vue";
+import { inject, onMounted, reactive } from "vue";
 export default {
   setup() {
     const $echarts = inject("echarts");
     const $http = inject("axios");
 
+    let data = reactive({});
+    let xdata = reactive([]);
+    let ydata = reactive([]);
+
+    function setData() {
+      xdata = data.data.chartOne.chartData.map((v) => v.title);
+      ydata = data.data.chartOne.chartData.map((v) => v.num);
+      console.log("xdata:", xdata);
+      console.log("ydata:", ydata);
+    }
+
     async function getState() {
-      const data = await $http({ url: "/1/data" });
-      console.log(data.data.chartOne.chartData);
+      data = await $http({ url: "/1/data" });
+      // console.log(data.data.chartOne.chartData);
     }
 
     onMounted(() => {
-      // 调用数据请求
-      getState();
       // 获取dom元素
-      const charts = $echarts.init(document.querySelector(".chart"));
-      // 配置echats
-      charts.setOption({
-        xAxis: {
-          type: "value",
-        },
-        yAxis: {
-          type: "category",
-        },
-        series: [
-          {
-            type: "bar",
+      const chart = $echarts.init(document.querySelector(".chart"));
+      // 调用方法
+      // 请求数据
+      getState().then(() => {
+        setData(); // 处理数据
+        // 配置echats
+        chart.setOption({
+          // 设置图表位置
+          grid: {
+            top: "3%",
+            left: "1%",
+            right: "6%",
+            bottom: "3%",
+            containLabel: true, // 包含坐标轴位置在图表中
           },
-        ],
+          xAxis: {
+            type: "value",
+          },
+          yAxis: {
+            type: "category",
+            data: xdata,
+          },
+          series: [
+            {
+              type: "bar",
+              data: ydata,
+              itemStyle: {
+                barBorderRadius: [0, 20, 20, 0],
+                color: new $echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                  {
+                    offset: 0,
+                    color: "#7F7FD5",
+                  },
+                  {
+                    offset: 0.5,
+                    color: "#86A8E7",
+                  },
+                  {
+                    offset: 1,
+                    color: "#91EAE4",
+                  },
+                ]),
+              },
+            },
+          ],
+        });
       });
     });
-    return {};
+    return {
+      getState,
+      data,
+      xdata,
+      ydata,
+      setData,
+    };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.chart {
+  height: 4.5rem;
+}
+h2 {
+  height: 0.6rem;
+  color: white;
+  line-height: 0.6rem;
+  font-size: 0.25rem;
+  text-align: center;
+}
+</style>
